5cc23cde48fc64d116d06a489e7f599854845529
Yuta123456 <yuuta09090530@icloud.com>
2022-07-04 02:15:02 +0900

    init

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <NuxtLink to="/">
+        <CButton>
+            toIndex
+        </CButton>
+    </NuxtLink>
+</div>
+</template>
+<script lang="js">
+import { CButton } from '@chakra-ui/vue'
+export default {
+  name: 'AboutPage',
+  components: {
+    CButton,
+  },
+//   methods: {
+//     ()
+//   }
+}
+</script>
