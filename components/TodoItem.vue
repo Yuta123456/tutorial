de8e129a3ea6292bba7c942c3ef4f001cea0253b
Yuta123456 <yuuta09090530@icloud.com>
2022-07-04 14:09:22 +0900

    todoItem実装

---
+++ components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <CBox>
+    HEY
+  </CBox>
+</template>
+
+<script>
+import { ITodoItem } from '../types/index'
+export default {
+  name: 'IndexPage',
+  components: {
+    CBox
+  },
+  props: {
+    todo: ITodoItem
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
